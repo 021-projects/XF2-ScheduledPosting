72760877865165ebc78fb04f2610ab324a78b81a
021-projects <devsellio@gmail.com>
2021-01-30 21:30:39 +0200

    v1.1.2 published

---
+++ upload/src/addons/BS/ScheduledPosting/_output/extension_hint.php
@@ -0,0 +1,49 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace BS\ScheduledPosting\XF\Entity
+{
+	class XFCP_Post extends \XF\Entity\Post {}
+	class XFCP_ProfilePost extends \XF\Entity\ProfilePost {}
+	class XFCP_Thread extends \XF\Entity\Thread {}
+	class XFCP_User extends \XF\Entity\User {}
+}
+
+namespace BS\ScheduledPosting\XF\Finder
+{
+	class XFCP_Post extends \XF\Finder\Post {}
+	class XFCP_ProfilePost extends \XF\Finder\ProfilePost {}
+	class XFCP_Thread extends \XF\Finder\Thread {}
+}
+
+namespace BS\ScheduledPosting\XF\Pub\Controller
+{
+	class XFCP_Forum extends \XF\Pub\Controller\Forum {}
+	class XFCP_Member extends \XF\Pub\Controller\Member {}
+	class XFCP_Post extends \XF\Pub\Controller\Post {}
+	class XFCP_ProfilePost extends \XF\Pub\Controller\ProfilePost {}
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
+}
+
+namespace BS\ScheduledPosting\XF\Service\Post
+{
+	class XFCP_Approver extends \XF\Service\Post\Approver {}
+	class XFCP_Editor extends \XF\Service\Post\Editor {}
+}
+
+namespace BS\ScheduledPosting\XF\Service\ProfilePost
+{
+	class XFCP_Approver extends \XF\Service\ProfilePost\Approver {}
+	class XFCP_Creator extends \XF\Service\ProfilePost\Creator {}
+	class XFCP_Editor extends \XF\Service\ProfilePost\Editor {}
+}
+
+namespace BS\ScheduledPosting\XF\Service\Thread
+{
+	class XFCP_Approver extends \XF\Service\Thread\Approver {}
+	class XFCP_Creator extends \XF\Service\Thread\Creator {}
+	class XFCP_Editor extends \XF\Service\Thread\Editor {}
+	class XFCP_Replier extends \XF\Service\Thread\Replier {}
+}
